8309cf6aaf6af607c42cd780a1b9c619fe279581
HONG <hmgcjn@gmail.com>
2022-07-13 16:07:35 +0900

    react-router-dom v6로 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/abot-us">
-          <h1>Hello</h1>
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/about-us" element={<h1>Hello</h1>} />
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
